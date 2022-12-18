535e0c9c62c0c92e83dc62053f9c5521feb0b669
Marcos A de S Silva <chuckymass@gmail.com>
2022-12-18 20:57:33 -0300

    Refatora módulo http para fazer a injeção de dependência dos casos de usos direto: SendNotification, GetAllNotifications

--- src/infra/http/controllers/notification.ts
+++ src/infra/http/controllers/notification.ts
@@ -1,20 +1,15 @@
 import { Body, Controller, Post, Get } from '@nestjs/common';
 import { NoParams } from '@application/entities/no-params';
-import { Usecase } from '@application/usecases/usecase';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
-import { SendNotificationResponse } from '@application/entities/interfaces/send-notification/response';
-import { SendNotificationRequest } from '@application/entities/interfaces/send-notification/request';
 import { NotificationViewModel } from '../view-model/notification-view-model';
-import { Notification } from '@application/entities/notification';
+import { SendNotification } from '@application/usecases/send-notification';
+import { GetAllNotifications } from '@application/usecases/get-all-notifications';
 
 @Controller('notifications')
 export class NotificationController {
   constructor(
-    private sendNotification: Usecase<
-      SendNotificationRequest,
-      SendNotificationResponse
-    >,
-    private getAllNotifications: Usecase<NoParams, Notification[]>,
+    private sendNotification: SendNotification,
+    private getAllNotifications: GetAllNotifications,
   ) {}
 
   @Get()
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,24 +3,10 @@ import { GetAllNotifications } from '@application/usecases/get-all-notifications
 import { SendNotification } from '@application/usecases/send-notification';
 import { DatabaseModule } from '@infra/database/database.module';
 import { NotificationController } from './controllers/notification';
-import { SendNotificationRequest } from '@application/entities/interfaces/send-notification/request';
-import { Usecase } from '@application/usecases/usecase';
-import { SendNotificationResponse } from '@application/entities/interfaces/send-notification/response';
-import { Notification } from '@application/entities/notification';
-import { NoParams } from '@application/entities/no-params';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [
-    {
-      provide: Usecase<SendNotificationRequest, SendNotificationResponse>,
-      useClass: SendNotification,
-    },
-    {
-      provide: Usecase<NoParams, Notification>,
-      useClass: GetAllNotifications,
-    },
-  ],
+  providers: [SendNotification, GetAllNotifications],
 })
 export class HttpModule {}
